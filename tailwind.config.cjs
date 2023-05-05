e1977c31d18f5478e898b54c03f7de2f10af5d26
Dave Nguyen <dave@haidave.com>
2023-05-05 23:22:18 +0200

    chore(styles): add custom css variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,16 @@ const config = {
       fontFamily: {
         sans: ['var(--font-sans)'],
       },
+      textColor: {
+        primary: 'var(--text-primary)',
+        secondary: 'var(-text-secondary)',
+        tertiary: 'var(--text-tertiary)',
+      },
+      backgroundColor: {
+        primary: 'var(--bg-primary)',
+        secondary: 'var(--bg-secondary)',
+        tertiary: 'var(--bg-tertiary)',
+      },
     },
   },
   plugins: [],
