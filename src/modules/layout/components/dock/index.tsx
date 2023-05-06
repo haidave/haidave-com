78a3da338ec8c6e77dd5007a661c2ef99b956f44
Dave Nguyen <dave@haidave.com>
2023-05-06 21:04:05 +0200

    feat(dock): add magnification effect

--- src/modules/layout/components/dock/index.tsx
+++ src/modules/layout/components/dock/index.tsx
@@ -1,14 +1,23 @@
+'use client'
+
 import { ROUTES } from '~/config/routes'
 import { DockItem } from './parts/dock-item'
+import { useMotionValue } from 'framer-motion'
 
 const Dock = () => {
+  const mouseX = useMotionValue(Infinity)
+
   return (
-    <footer className="fixed bottom-8 left-1/2 -translate-x-1/2 rounded-full border">
-      <ul className="flex gap-2">
-        <DockItem href={ROUTES.home} shortcut="1" />
-        <DockItem href={ROUTES.about} shortcut="2" />
-        <DockItem href={ROUTES.resources} shortcut="3" />
-        <DockItem href={ROUTES.notes} shortcut="4" />
+    <footer className="fixed bottom-8 left-1/2 -translate-x-1/2">
+      <ul
+        onMouseMove={(e) => mouseX.set(e.pageX)}
+        onMouseLeave={() => mouseX.set(Infinity)}
+        className="flex h-16 items-end gap-2 rounded-full border px-4 pb-3"
+      >
+        <DockItem href={ROUTES.home} shortcut="1" mouseX={mouseX} />
+        <DockItem href={ROUTES.about} shortcut="2" mouseX={mouseX} />
+        <DockItem href={ROUTES.resources} shortcut="3" mouseX={mouseX} />
+        <DockItem href={ROUTES.notes} shortcut="4" mouseX={mouseX} />
       </ul>
     </footer>
   )
--- src/modules/layout/components/dock/parts/dock-item/index.tsx
+++ src/modules/layout/components/dock/parts/dock-item/index.tsx
@@ -1,43 +1,77 @@
 'use client'
 
-import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useRouter, usePathname } from 'next/navigation'
+import { type MotionValue, motion, useTransform, useSpring, useAnimationControls } from 'framer-motion'
 
 import { cn } from '~/lib/utils'
 
 type DockItemProps = {
   href: string
   shortcut: string
+  mouseX: MotionValue
 }
 
-const DockItem: React.FC<DockItemProps> = ({ href, shortcut }) => {
+const DockItem: React.FC<DockItemProps> = ({ href, shortcut, mouseX }) => {
   const router = useRouter()
   const pathname = usePathname()
   const isActive = pathname === href
+
+  const ref = useRef<HTMLButtonElement>(null)
+  const controls = useAnimationControls()
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
       if (event.key === shortcut) {
         router.push(href)
+        void controls.start(() => ({ translateY: [0, -30, 0] }))
       }
     }
 
     window.addEventListener('keypress', handleKeyPress)
-  }, [router, href, shortcut])
+  }, [router, href, shortcut, controls])
+
+  const distance = useTransform(mouseX, (val) => {
+    const bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 }
+
+    return val - bounds.x - bounds.width / 2
+  })
+  const widthSync = useTransform(distance, [-100, 0, 100], [40, 80, 40])
+  const width = useSpring(widthSync, { damping: 12, mass: 0.1, stiffness: 120 })
 
   return (
-    <li
-      className={cn(
-        'flex aspect-square w-10 justify-center rounded-full',
-        'hover:bg-tertiary',
-        isActive ? 'bg-tertiary font-bold text-primary' : ''
-      )}
+    <motion.li
+      className="relative"
+      onClick={() => {
+        router.push(href)
+        void controls.start(() => ({ translateY: [0, -30, 0] }))
+      }}
     >
-      <Link href={href} className="grid h-full w-full place-items-center">
+      <motion.button
+        ref={ref}
+        style={{ width }}
+        animate={controls}
+        transition={{
+          default: {
+            duration: 0.2,
+          },
+          translateY: {
+            duration: 0.4,
+            ease: 'easeInOut',
+            times: [0, 0.5, 1],
+          },
+        }}
+        whileTap={{ scale: isActive ? 1 : 0.8 }}
+        className={cn(
+          'grid aspect-square w-10 place-items-center rounded-full bg-tertiary',
+          'hover:bg-secondary',
+          isActive ? 'font-bold' : ''
+        )}
+      >
         {shortcut}
-      </Link>
-    </li>
+      </motion.button>
+      {isActive ? <div className="absolute -bottom-2 left-1/2 h-1 w-1 rounded-full bg-[#7e7e7e]" /> : null}
+    </motion.li>
   )
 }
 
