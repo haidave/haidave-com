e05c3d28abe2ce75dc0f2bbd9725785789324ffc
Dave Nguyen <dave@haidave.com>
2023-06-08 22:55:08 +0200

    chore(utils): add copyToClipboard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]): string {
+const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(inputs))
 }
+
+const copyToClipboard = async (text: string) => {
+  try {
+    await navigator.clipboard.writeText(text)
+  } catch {
+    console.error('Failed to copy to clipboard')
+  }
+}
+
+export { cn, copyToClipboard }
